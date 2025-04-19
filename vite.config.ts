464d3c399416cdd3545afac8e7bb16768080eae1
ntokozodev <ntokozodev@gmail.com>
2025-04-19 22:50:37 +0200

    Add tests coverage and thresholds

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  plugins: [dts({ tsconfigPath: './tsconfig.build.json' })],
   build: {
     lib: {
-      entry: './src/index.ts',
-      name: 'MyLibrary',
-      fileName: (format) => `my-library.${format}.js`,
+      entry: 'src/index.ts',
+      name: 'Typescript data sanitizer',
+      fileName: 'ts-simple-mapper',
+      formats: ['es', 'cjs'],
     },
-  },
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {}
+      }
+    }
+  }
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,22 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text'],
+      exclude: [
+        'node_modules/',
+        'dist/',
+        '**/*.d.ts',
+        '**/*.test.ts',
+        '**/*.config.ts'
+      ],
+      thresholds: {
+        lines: 90,
+        functions: 90,
+        branches: 90,
+        statements: 90
+      }
+    }
   },
 });
