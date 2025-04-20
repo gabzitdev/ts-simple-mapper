8e977ae7b362461ec3d5630c3a09ca7ed94888fd
ntokozodev <ntokozodev@gmail.com>
2025-04-20 16:28:35 +0200

    Fix types export mismatch

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [dts({ tsconfigPath: './tsconfig.build.json' })],
+  plugins: [
+    dts({
+      tsconfigPath: './tsconfig.build.json',
+      include: ['src/**/*.ts'], // Ensure all TypeScript files in src are included
+    }),
+  ],
   build: {
     lib: {
       entry: 'src/index.ts',
@@ -13,8 +18,8 @@ export default defineConfig({
     rollupOptions: {
       external: [],
       output: {
-        globals: {}
-      }
-    }
-  }
+        globals: {},
+      },
+    },
+  },
 });
