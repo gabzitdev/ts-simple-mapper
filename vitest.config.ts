9977cf0070ffde722b1c5a33b2c46e319a88ef99
ntokozodev <ntokozodev@gmail.com>
2025-04-20 13:54:33 +0200

    Install husky and setup pre-hooks

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
         'dist/',
         '**/*.d.ts',
         '**/*.test.ts',
-        '**/*.config.ts'
+        '**/*.config.*'
       ],
       thresholds: {
         lines: 90,
